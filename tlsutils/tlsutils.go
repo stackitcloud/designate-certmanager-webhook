17017b49f8ea801e1f414e9e4221f0c928e0c0e0
Marcel Boehm <marcel.boehm@inovex.de>
2025-08-07 09:13:49 +0200

    Remove dependency to external-dns (#31)

---
+++ tlsutils/tlsutils.go
@@ -0,0 +1,5 @@
+// package tlsutils was copied from
+// https://github.com/kubernetes-sigs/external-dns/tree/0e7c3af22109aefa8664bfcd5ae6593247078ee3/pkg/tlsutils in order
+// to avoid a dependency on the external-dns package for this helper, which is not really tied to external-dns and
+// unlikely to ever change.
+package tlsutils
